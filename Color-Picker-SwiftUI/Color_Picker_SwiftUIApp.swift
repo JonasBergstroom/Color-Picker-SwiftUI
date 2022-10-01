de59d4b7276d4b513ad47ac0b9dd3809f5c7030d
jonasbergstrom <jontrian97@gmail.com>
2022-10-01 14:44:21 +0200

    Initial Commit

---
+++ Color-Picker-SwiftUI/Color_Picker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Picker_SwiftUIApp.swift
+//  Color-Picker-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-01.
+//
+
+import SwiftUI
+
+@main
+struct Color_Picker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color-Picker-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Color-Picker-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
